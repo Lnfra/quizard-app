aac463d30b09ea6587af86d3e091f920d5b53a15
Kailin <kailin.tan.2010@gmail.com>
2019-03-09 22:32:57 +0800

    Add semantic ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//right on top
+import MenuExample from './MenuExample.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <MenuExample />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
